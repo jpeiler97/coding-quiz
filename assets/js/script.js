7ddb97d8d9b0e764a12a3176dfcaf2ade24f0c4b
JP Eiler <jp.eiler97@gmail.com>
2021-03-03 10:21:18 -0600

    added scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//QUERY SELECTORS
 var highScoreButton = document.querySelector('.high-scores');
 var timeText = document.querySelector('.timer-text');
 var startArea = document.querySelector('.start-area');
@@ -8,11 +9,14 @@ var questionText = document.querySelector('.question');
 var correctText = document.querySelector('.correct-status');
 var answerChoices = document.querySelector('.answer-choices');
 var answerButton = document.querySelector('.answer');
+var scoreText = document.querySelector('.score-text');
 
 //Number of seconds to complete quiz
-var seconds = 60;
+var seconds = 15;
 //Number question in index
 var questionIndex = 0;
+//Score variable
+var score = 0;
 
 //Array of objects representing questions and their answers
 var questions = [
@@ -36,35 +40,47 @@ var questions = [
 //Gets new question
 function getQuestion() {
 	//Defines and displays question based on index
-
 	var questionObject = questions[questionIndex];
 	questionText.textContent = questionObject.question;
 
 	//Gets answers and answer choices from questionObject to compare
 	var answers = questionObject.possibleAnswers;
 
+	//Fills buttons in game area with answers
 	for (i = 0; i < answers.length; i++) {
 		answerChoices.children[i].textContent = answers[i];
 		answerChoices.children[i].addEventListener('click', checkQuestion);
 	}
 }
 
+//Checks if answer selected is correct or incorrect, and if there are any questions left
 function checkQuestion() {
-	if (questionIndex < questions.length - 1) {
+	//Checks if there are no more questions, or if the timer ran out due to a question being wrong
+	if (questionIndex === questions.length - 1 || seconds <= 0) {
+		//Ensures the score gets incremented if the last question is correct
+		if (event.target.textContent === questions[questionIndex].answer) {
+			score++;
+		}
+		endGame();
+	} else if (questionIndex < questions.length - 1) {
+		//If there are questions left, this compares selected answer to actual answer
+		//Upon selection of an answer, the index of the questions array is incremented by 1 to get the
+		//next question in the array, the user is told if the answer is correct, and the next question is displayed.
 		if (event.target.textContent === questions[questionIndex].answer) {
 			correctText.textContent = 'Correct!';
-			console.log('correct');
+			score++;
 			questionIndex++;
 			getQuestion();
 		} else if (event.target.textContent !== questions[questionIndex].answer) {
 			correctText.textContent = 'Incorrect!';
-			console.log('incorrect');
 			seconds -= 10;
 			questionIndex++;
-			getQuestion();
+			//Ensures that if the incorrect answer penalty causes the clock to go below zero, then the
+			//next question will display.
+			if (seconds > 0) {
+				getQuestion();
+			}
 		}
-	} else if (questionIndex === questions.length - 1) {
-		endGame();
 	}
 }
 
@@ -86,6 +102,9 @@ function startGame() {
 function endGame() {
 	quizArea.style.display = 'none';
 	endArea.style.display = 'block';
+	scoreText.textContent = score;
+	//Resets questionIndex for replay
+	questionIndex = 0;
 }
 
 //Function to count down timer one second at a time
@@ -95,9 +114,11 @@ function setTime() {
 		timeText.textContent = seconds;
 
 		//Ends game when timer reaches 0
-		if (seconds === 0) {
+		if (seconds <= 0) {
 			clearInterval(timeInterval);
 			endGame();
+			seconds = 0;
+			timeText.textContent = seconds;
 		}
 	}, 1000);
 }
